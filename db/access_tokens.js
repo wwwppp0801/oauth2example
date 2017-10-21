94cfd869512a0d66a2fc526ae97154da8729b797
wp@rmbp <wwwppp0801@qq.com>
2017-10-21 19:02:48 +0800

    fix

--- db/access_tokens.js
+++ db/access_tokens.js
@@ -8,13 +8,15 @@ module.exports.find = (key, done) => {
 };
 
 module.exports.findByUserIdAndClientId = (userId, clientId, done) => {
-  for (let token of tokens) {
-    if (tokens[token].userId === userId && tokens[token].clientId === clientId) return done(null, token);
+    for (let token in tokens) {
+        if(tokens.hasOwnProperty(token)){
+            if (tokens[token].userId === userId && tokens[token].clientId === clientId) return done(null, token);
+        }
   }
   return done(new Error('Token Not Found'));
 };
 
 module.exports.save = (token, userId, clientId, done) => {
-  tokens[token] = { userId, clientId };
+  tokens[token] = { userId:userId, clientId:clientId };
   done();
 };
